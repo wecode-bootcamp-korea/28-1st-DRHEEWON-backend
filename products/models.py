e24110decb2d9821944bf90a32495661e5ce22cc
kimjy10 <kjy120924@gmail.com>
2021-12-29 16:51:43 +0900

    complete initial setting for signin feature

--- products/models.py
+++ products/models.py
@@ -10,33 +10,33 @@ class Product(models.Model):
     launched_at       = models.DateTimeField()
 
     class Meta:
-        db_tables = "products"
+        db_table = "products"
 
 class ProductImage(models.Model):
     url     = models.CharField(max_length=1000)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     class Meta:
-        db_tables = "product_images"
+        db_table = "product_images"
 
 class Size(models.Model):
     name = models.CharField(max_length=20)
 
     class Meta:
-        db_tables = "sizes"
+        db_table = "sizes"
 
 class Color(models.Model):
     color = models.CharField(max_length=20)
 
     class Meta:
-        db_tables = "colors"
+        db_table = "colors"
 
 class ProductOption(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     size    = models.ForeignKey(Size, on_delete=models.CASCADE)
     color   = models.ForeignKey(Color, on_delete=models.CASCADE)
-    price   = models.DecimalField()
+    price   = models.DecimalField(max_digits=30, decimal_places=5)
     stock   = models.IntegerField()
 
     class Meta:
-        db_tables = "product_options"
+        db_table = "product_options"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
+import datetime
 
-# Create your views here.
+import jwt
+import bcrypt
+
+from django.shortcuts import render
+from django.views     import View
+
+from dr_martens.checkitem import CheckItem
+
+class SignInView(View):
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            signin_key_list=["user_id", "password"]
+            CheckItem.check_keys_in_body(data, signin_key)
+        except:
+            pass
