eaa8aec163345333a20b9e34b718273c2deea477
이지엽 <vvvv1541@naver.com>
2021-12-31 11:43:42 +0900

    signup logout

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from users.views import SignInView
+from users.views import SignInView, LogOutView, SignUpView
 
 urlpatterns = [
+        path('/signup', SignUpView.as_view()),
         path('/signin', SignInView.as_view()),
+        path('/logout', LogOutView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 import json
 import datetime
 
+from django.http.response import HttpResponse, HttpResponseRedirect
+
 import jwt
 import bcrypt
 
@@ -10,9 +12,33 @@ from django.conf      import settings
 
 from users.models         import User
 from dr_martens.checkitem import CheckItem
+from django.contrib import auth
+
+class SignUpView(View):
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            
+            if User.objects.filter(user_id=data['user_id']).exists():
+                return JsonResponse({'message' : 'user exists'}, status=400)
+
+            password = data['password']     
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        
+            User.objects.create(
+                username      = data['username'],
+                user_id       = data['user_id'],
+                password      = hashed_password,
+                mobile_number = data['mobile_number'],
+                email         = data['email'],
+                point         = data['point'],
+            )
+            return JsonResponse({'message' : 'SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({'message' : 'KeyError'}, status=400)
 
 class SignInView(View):
-
     def post(self, request):
         try:
             data = json.loads(request.body)
@@ -43,3 +69,8 @@ class SignInView(View):
 
         except User.DoesNotExist:
             return JsonResponse({'message':'INVALID_USER'}, status=401)
+
+class LogOutView(View):
+    def logout(request):
+        auth.logout(request)
+        return HttpResponseRedirect('home')
