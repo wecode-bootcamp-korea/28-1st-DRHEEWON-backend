edf8374c8153f31bc8cb8189125beab4fc7bddb8
kimjy-par <kimjy@DESKTOP-M7IUM0V.localdomain>
2021-12-31 18:37:22 +0900

    modifying users view following mentor's advices

--- users/views.py
+++ users/views.py
@@ -12,30 +12,28 @@ from users.models         import User
 from dr_martens.checkitem import CheckItem
 
 class SignInView(View):
-
     def post(self, request):
         try:
             data = json.loads(request.body)
 
-            signin_key_list = ["user_id", "password"]
-            CheckItem.check_keys_in_body(data, signin_key_list)
+            required_keys = ["user_id", "password"]
+            CheckItem.check_keys_in_body(data, required_keys)
 
             user_id  = data["user_id"]
             password = data["password"].encode('utf-8')
 
-            user = User.objects.get(user_id = user_id)
-            
+            user           = User.objects.get(user_id = user_id)
             valid_password = user.password.encode('utf-8')
 
             if not bcrypt.checkpw(password, valid_password):
                 return JsonResponse({'message':'INVALID_USER'}, status=401)
 
-            exp_date = datetime.datetime.utcnow() + datetime.timedelta(hours=2)
+            exp_date     = datetime.datetime.utcnow() + datetime.timedelta(hours=2)
             access_token = jwt.encode(
                     {'id':user.id, 'exp':exp_date},
                     settings.SECRET_KEY,
                     algorithm=settings.ALGORITHM,
-                    )
+            )
             return JsonResponse({'message':'success','token':access_token}, status=200)
 
         except KeyError as e:
