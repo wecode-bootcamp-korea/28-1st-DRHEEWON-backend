5ba38f174baba432bea0e56008dbc81631b9ea05
이지엽 <vvvv1541@naver.com>
2022-01-02 19:07:19 +0900

    signup complete

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,4 @@ from users.views import SignInView, LogOutView, SignUpView
 
 urlpatterns = [
         path('/signup', SignUpView.as_view()),
-        path('/signin', SignInView.as_view()),
-        path('/logout', LogOutView.as_view()),
-]
+        ]
