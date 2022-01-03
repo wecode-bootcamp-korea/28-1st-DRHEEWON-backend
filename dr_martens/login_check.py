8e6d59728587242306bf392d0507cc488f58c096
kimjy-par <kimjy@DESKTOP-M7IUM0V.localdomain>
2022-01-03 19:19:27 +0900

    modifying login decorator

--- dr_martens/login_check.py
+++ dr_martens/login_check.py
@@ -8,31 +8,18 @@ SECRET_KEY = settings.SECRET_KEY
 ALGORITHM  = settings.ALGORITHM
 
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-
-    return ip
-
 def login_check(func):
     def wrapper(self, request, *args, **kwargs):
         try:
             access_token = request.headers.get("Authorization", None)
-
-            if access_token:
-                payload      = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
-                user         = User.objects.get(id=payload.get('id'))
-                request.user = user
-            else:
-                request.user = None
-
-            request.client_ip = get_client_ip(request)
+            payload      = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
+            user         = User.objects.get(id=payload.get('id'))
+            request.user = user
 
             return func(self, request, *args, **kwargs)
+
+        except KeyError as e:
+            return JsonResponse({'message':getattr(e,'message',str(e))}, status=401)
 
         except jwt.ExpiredSignatureError:
             return JsonResponse({'message':'TOKEN_EXPIRED'}, status=400)
@@ -41,4 +28,3 @@ def login_check(func):
             return JsonResponse({'message':'UserDoesNotExist'}, status=400)
 
     return wrapper
-
