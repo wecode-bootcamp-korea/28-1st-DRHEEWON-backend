b2b8ac5c7ac2cce9526bcb299afae18320d322fb
kimjy-par <kjy120924@gmail.com>
2022-01-03 19:59:05 +0900

    modifying databases

--- products/models.py
+++ products/models.py
@@ -48,7 +48,7 @@ class ProductOption(models.Model):
     product      = models.ForeignKey(Product, on_delete=models.CASCADE)
     size         = models.ForeignKey(Size, on_delete=models.CASCADE)
     color        = models.ForeignKey(Color, on_delete=models.CASCADE)
-    subcategory  = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    subcategory  = models.ForeignKey(Subcategory, on_delete=models.CASCADE, null=True)
     price        = models.DecimalField(max_digits=30, decimal_places=5)
     stock        = models.IntegerField()
 
